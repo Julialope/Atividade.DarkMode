ecac6edb783ae01439ef8f88f7479f6c1e1d2bd7
Julia Lopes <126586602+Julialope@users.noreply.github.com>
2023-10-03 23:41:53 -0300

    Add files via upload

---
+++ DarkMode/script.js
@@ -0,0 +1,10 @@
+function ModoEscuro(button) {
+  var square = document.getElementById("square");
+  square.style.backgroundColor = "#000000";
+  square.style.color = "white";
+
+  var DarkMOde = document.getElementById("txtNome", "txtTelefone", "txtOrigem", "txtDataContato", "txtObservacao");
+  DarkMOde.style.backgroundColor = "#1C1C1C";
+  DarkMOde.style.color = "white";
+}
+
